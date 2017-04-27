5cc8043fac2751bbdcf954e92eb22c21763cbe60
Sven Erik Knop <sknop@perforce.com>
2017-04-27 11:15:33 +0100

    Without debug output

--- src/main/resources/perforce/ReleaseTrigger.py
+++ src/main/resources/perforce/ReleaseTrigger.py
@@ -8,6 +8,7 @@ import sys
 from com.perforce.p4java.server import ServerFactory
 from com.perforce.p4java.option.server import GetChangelistsOptions
 from com.perforce.p4java.core import IChangelist
+from com.perforce.p4java.core.file import FileSpecBuilder
 
 if perforceServer is None:
    sys.exit(1)
@@ -23,18 +24,19 @@ opts = GetChangelistsOptions()
 opts.setMaxMostRecent(1)
 opts.setType(IChangelist.Type.SUBMITTED)
 
+filespec = None
 if workspace:
-    opts.setClientName(workspace)
+    client = iServer.getClient(workspace)
+    iServer.setCurrentClient(client)
+    filespec = FileSpecBuilder.makeFileSpecList("//"+workspace+"/...")
 
-changelists = iServer.getChangelists(None, opts)
+changelists = iServer.getChangelists(filespec, opts)
 
 iServer.disconnect()
 
 changelistId = str(0)
 for cl in changelists:
   changelistId = str(cl.id)
-  with open('/tmp/trigger.out', 'w+') as f:
-      f.write("Latest change : {}\n".format(changelistId))
 
 triggerState = changelistId
 
